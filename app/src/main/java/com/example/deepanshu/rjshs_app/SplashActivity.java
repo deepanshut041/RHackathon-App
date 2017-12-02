2a804255754544df97c9525950b59ecc897fc995
deepanshut041 <deepanshut041@gmail.com>
2017-12-03 02:00:05 +0530

    Android splash screen added with login

--- app/src/main/java/com/example/deepanshu/rjshs_app/SplashActivity.java
+++ app/src/main/java/com/example/deepanshu/rjshs_app/SplashActivity.java
@@ -1,13 +1,31 @@
 package com.example.deepanshu.rjshs_app;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        button = (Button) findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                    }
+                }, 2000);
+            }
+        });
     }
 }
